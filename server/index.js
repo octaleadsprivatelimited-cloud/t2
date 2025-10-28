3ccc71941befe228ee133af6de6e87e64001e201
Octa Leads <octaleadsprivatelimited@gmail.com>
2025-10-28 17:06:05 +0530

    Render: install server deps in postinstall; use PORT; reliable build/start

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const path = require('path');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 3000;
 
 // Security middleware
 app.use(helmet({
